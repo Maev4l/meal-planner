322a6f7f88d55837bf9eb2fc8e696b012bcf0ca7
Maev4l <maeval.nightingale@gmail.com>
2026-05-03 12:08:16 +0200

    chore: Enable force delete flag

--- packages/infrastructure/webclient.tf
+++ packages/infrastructure/webclient.tf
@@ -2,6 +2,8 @@
 
 resource "aws_s3_bucket" "webclient" {
   bucket = "meal-planner-web-client"
+  # Allow `terraform destroy` to remove the bucket even when objects remain.
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_public_access_block" "webclient" {
